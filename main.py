53d7be5f03c6f18ba98de16b06d428517f71dc8f
sackeovas <120979637+sackeovas@users.noreply.github.com>
2024-04-08 15:40:33 +0000

    Základ

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+import sys, random, pygame
+from pygame.locals import *
+
+
+
+    
+def terminate():
+    """Exit the program."""
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
